32cae6f7e825c131dc3ceaa447518801df6f37a7
justinshin6 <justinshin@brown.edu>
2022-07-25 14:26:28 -0400

    complete spotify

--- beautiful-soup-intro/billboard-spotify-data/main.py
+++ beautiful-soup-intro/billboard-spotify-data/main.py
@@ -1,28 +1,89 @@
 from bs4 import BeautifulSoup
 import requests
-import re
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from decouple import config
 
-regex = re.compile("[0-9]{4}\-[0-1][0-9]\-[0-9]{2}")
+'''
+Program that gets a date from the user and creates a Spotify playlist of the top 100 songs according 
+to Billboard Music at that specific date. 
+'''
 
-def check_date_format(date):
-    match = re.match(regex, date)
+# set up Client_ID and Client_Secret using config
+CLIENT_ID = config('CLIENT_ID')
+CLIENT_SECRET = config('CLIENT_SECRET')
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-public",
+        redirect_uri="https://example.com",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="beautiful-soup-intro/billboard-spotify-data/token.txt"
+    )
+)
+# user id
+user_id = sp.current_user()["id"]
 
-    if (match):
-        return date
+# check to see if playlist name already in Spotify library 
+playlists = sp.user_playlists(user=user_id)['items']
+existing_playlist_names = []
+for i in range(len(playlists)):
+    existing_playlist_names.append(playlists[i]['name'])
 
-    else: 
-       return check_date_format(input("Invalid date, try again: "))
+# check whether or not the year is a valid year 
+valid_year = False
+while not valid_year:
+    # get the user input date
+    user_input_data = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+    curr_year = user_input_data.split('-')[0]
+    PLAYLIST_NAME = f'Billboard Top 100 Playlist {user_input_data}'
 
-user_input_data = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+    if PLAYLIST_NAME not in existing_playlist_names:
+        valid_year = True
+    else:
+        print(f"The playlist name {PLAYLIST_NAME} is already in your library. Try another date.")
 
-print(check_date_format(user_input_data))
-
+# set up variables 
+PLAYLIST_DESCRIPTION = f'This is a playlist that contains the top 100 songs on the Billboard top 100 \
+    on the date {user_input_data}'
 URL = f'https://www.billboard.com/charts/hot-100/{user_input_data}/'
 response = requests.get(URL)
 billboard_webpage = response.text
-
 soup = BeautifulSoup(billboard_webpage, 'html.parser')
 
-# get the titles from the BillBoard website
+# get the titles from the BillBoard website and get song_uri from the titles
 title_tags = soup.find_all(name='h3', id='title-of-a-story', class_='a-no-trucate')
 titles = [title.getText().strip() for title in title_tags]
+song_uris = []
+for title in titles:
+    result = sp.search(q=f"track:{title} year:{curr_year}", type="track")
+    try:
+        uri = result['tracks']['items'][0]['uri']
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{title} not found in Spotify. Skipped")
+
+
+# if there are available songs to add, then we add a spotify playlist 
+if len(song_uris) > 0:
+
+    # create a new spotify playlist
+    playlist = sp.user_playlist_create(
+        user=user_id,
+        name=PLAYLIST_NAME,
+        public= True,
+        description=PLAYLIST_DESCRIPTION,
+    )
+
+    # get the id of the playlist and then add the song_uri's of the current year to the playlist
+    playlist_id = playlist['id']
+
+    sp.playlist_add_items(
+        playlist_id=playlist_id,
+        items=song_uris,
+    )
+    print(f"Succesfully created {PLAYLIST_NAME} for the date {user_input_data}!!")
+else:
+    # if there are no songs to add, then that means the year is not valid. 
+    print(f"There seems to be no songs for the date {user_input_data}. Try again.")
--- beautiful-soup-intro/top-100-movies-data/main.py
+++ beautiful-soup-intro/top-100-movies-data/main.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests 
 
+
+
 URL = 'https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/'
 DIRECTORY_FILE_PATH = 'top-100-movies-data/'
 
