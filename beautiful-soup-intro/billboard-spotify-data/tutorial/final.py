cdcc3a0861bb9392fc3c3994ef8ce33e6d75ace1
justinshin6 <justinshin@brown.edu>
2022-12-12 11:15:26 -0500

    completed workshop billboard

--- beautiful-soup-intro/billboard-spotify-data/tutorial/final.py
+++ beautiful-soup-intro/billboard-spotify-data/tutorial/final.py
@@ -1,7 +1,6 @@
 from pprint import pprint
 
 import spotipy
-import spotipy.util as util
 from spotipy.oauth2 import SpotifyOAuth
 from bs4 import BeautifulSoup
 import requests
@@ -10,16 +9,19 @@ URL = "https://www.billboard.com/charts/hot-100/"
 
 date = input("Which year do you want to go for billboard hot 100? Type the date in YYYY-MM-DD format: ")
 
-year = date.split("-")[0]
 response = requests.get(URL + date)
 soup = BeautifulSoup(response.text, 'html.parser')
+
+# web scrape to find all of the song names from the top 100 
 song_names = soup.find_all(name="h3", id="title-of-a-story", class_="a-no-trucate")
 song_list = [song.getText().strip("\n\t") for song in song_names]
+year = date.split("-")[0]
 
+# environment variables 
 CLIENT_ID = config("CLIENT_ID")
 CLIENT_SECRET = config("CLIENT_SECRET")
-print("I'm now Here")
 
+# authenticate with spotipy, set redirect URI to http://example.com and make sure to do it in developer dashboard as well 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
@@ -30,25 +32,31 @@ sp = spotipy.Spotify(
         cache_path="token.txt"
     )
 )
-print("I'm Here")
+print("Successfully authenticated! ")
+
+# grab your user_id 
 user_id = sp.current_user()["id"]
 
 song_uris = []
 
+# loop through song list and then add song uris to song_uris array 
 for i in range(len(song_list)):
     result = sp.search(q=f"track:{song_list[i]} year:{year}", type="track")
+
+    # keep track of current song that is being downloaded
     print(f"Loading Song {i}:")
     try:
         uri = result["tracks"]["items"][0]["uri"]
         song_uris.append(uri)
     except IndexError:
-        continue
-        print(f"{song} doesn't exist in Spotify. Skipped.")
+        print(f"{song_list[i]} doesn't exist in Spotify. Skipped.")
 
 print("Finished! Starting to Make Playlist")
-playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
-# print(playlist)
 
+# creating the actual spotify playlist
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+
+# adding song uris to the playlist 
 sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
 
 print("Playlist made! ")
