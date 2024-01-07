2c714f6b2c69b1433d76bf470a6bb5bbb678ee61
justinshin6 <justinshin@brown.edu>
2024-01-07 15:52:39 -0500

    dynamic templates

--- django/studybud/base/views.py
+++ django/studybud/base/views.py
@@ -7,10 +7,24 @@ Views take HTTP requests and returns HTTP responses. Views are usually
 put in a file called views.py located in each app's respective folder 
 '''
 
+# variable that keeps track of the rooms that we have 
+rooms = [
+    {"id": 1, 'name': "Let's learn python!"},
+    {"id": 2, 'name': "Design with me "}, 
+    {"id": 3, 'name': "Frontend stuff"},
+    
+]
+
 # home route 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 # room route 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    context = {}
+    for r in rooms:
+        if r["id"] == int(pk):
+            context["room"] = r
+
+    return render(request, 'base/room.html', context)
