a971043af13a2a3822e0c81ee681b7d624769589
justinshin6 <justinshin@brown.edu>
2024-01-09 15:45:18 -0500

    introduce migrations

--- django/studybud/base/models.py
+++ django/studybud/base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# model -> table are synonymous 
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants = 
+    updated = models.DateTimeField(auto_now=True) # take timestamp every time updated 
+    created = models.DateTimeField(auto_now_add=True) # only when created instance
+
+    def __str__(self):
+        return self.name 
--- django/studybud/base/views.py
+++ django/studybud/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Room 
 # Create your views here.
 '''
 Views take HTTP requests and returns HTTP responses. Views are usually
@@ -8,23 +8,23 @@ put in a file called views.py located in each app's respective folder
 '''
 
 # variable that keeps track of the rooms that we have 
-rooms = [
-    {"id": 1, 'name': "Let's learn python!"},
-    {"id": 2, 'name': "Design with me "}, 
-    {"id": 3, 'name': "Frontend stuff"},
+# rooms = [
+#     {"id": 1, 'name': "Let's learn python!"},
+#     {"id": 2, 'name': "Design with me "}, 
+#     {"id": 3, 'name': "Frontend stuff"},
     
-]
+# ]
 
 # home route 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 # room route 
 def room(request, pk):
+    room = Room.objects.get(id=pk)
     context = {}
-    for r in rooms:
-        if r["id"] == int(pk):
-            context["room"] = r
+    context["room"] = room
 
     return render(request, 'base/room.html', context)
